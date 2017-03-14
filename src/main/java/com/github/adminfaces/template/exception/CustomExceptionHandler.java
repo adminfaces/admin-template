e80c75b50d83c0f53706f9ca2510df3cf1dae060
rmpestano <bigboss666>
2017-03-13 23:10:26 -0300

    back on requestScope for error pages

--- src/main/java/com/github/adminfaces/template/exception/CustomExceptionHandler.java
+++ src/main/java/com/github/adminfaces/template/exception/CustomExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.github.adminfaces.template.exception;
 
-import com.github.adminfaces.template.bean.AdminExceptionMB;
-import com.github.adminfaces.template.model.AdminException;
 import com.github.adminfaces.template.util.Constants;
 import org.omnifaces.config.WebXml;
 import org.omnifaces.util.Exceptions;
@@ -21,10 +19,10 @@ import javax.faces.event.PhaseId;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Iterator;
 
 import static com.github.adminfaces.template.util.Assert.has;
+import static javax.servlet.RequestDispatcher.*;
 
 
 /**
@@ -55,7 +53,6 @@ public class CustomExceptionHandler extends ExceptionHandlerWrapper {
         handleException(context);
         wrapped.handle();
     }
-
 
     /**
      * @param context
@@ -98,9 +95,12 @@ public class CustomExceptionHandler extends ExceptionHandlerWrapper {
         }
 
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        AdminExceptionMB adminExceptionMB = context.getApplication().evaluateExpressionGet(context, "#{adminExceptionMB}", AdminExceptionMB.class);
-        AdminException adminException = new AdminException(e, request.getHeader("Referer"), "" + HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        adminExceptionMB.create(adminException);
+        request.setAttribute(ERROR_EXCEPTION + "_stacktrace", e);
+        request.setAttribute(ERROR_EXCEPTION_TYPE, e.getClass().getName());
+        request.setAttribute(ERROR_MESSAGE, e.getMessage());
+        request.setAttribute(ERROR_REQUEST_URI, request.getHeader("Referer"));
+        request.setAttribute(ERROR_STATUS_CODE, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
         String errorPage = findErrorPage(e);
         if (!has(errorPage)) {
             String errorPageParam = context.getExternalContext().getInitParameter(Constants.InitialParams.ERROR_PAGE);
@@ -108,14 +108,7 @@ public class CustomExceptionHandler extends ExceptionHandlerWrapper {
                 errorPage = Constants.DEFAULT_ERROR_PAGE;
             }
         }
-        //context.getApplication().getNavigationHandler().handleNavigation(context, null, errorPage); //do not work on android webview
-        String urlExtension = context.getViewRoot().getViewId().substring(context.getViewRoot().getViewId().indexOf(".") + 1);
-        String pageToRedirect = errorPage.substring(0,errorPage.indexOf(".") + 1) + urlExtension;
-        try {
-            context.getExternalContext().redirect(context.getExternalContext().getRequestContextPath()+pageToRedirect);
-        } catch (Exception e1) {
-              logger.error("Could not redirect to page " + context.getExternalContext().getRequestContextPath()+pageToRedirect, e);
-        }
+        context.getApplication().getNavigationHandler().handleNavigation(context, null, errorPage);
         context.renderResponse();
     }
 
