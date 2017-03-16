16d4ee385f5a66d75b0e4b690c236cddb72bfcd2
rmpestano <rmpestano@gmail.com>
2017-03-16 19:55:01 -0300

    perfumery

--- src/main/resources/META-INF/resources/js/admintemplate.js
+++ src/main/resources/META-INF/resources/js/admintemplate.js
@@ -206,3 +206,12 @@ function isMobile() {
     var mq = window.matchMedia("(max-width: 767px)");
     return (typeof mq != 'undefined' && mq.matches);
 }
+
+
+function activateScrollToTop(){
+    if(isMobile() && window.pageYOffset > 400) {
+        $('#scrollTop').show(500);
+    } else {
+        $('#scrollTop').hide(500);
+    }
+}
