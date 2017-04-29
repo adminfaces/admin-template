2c2d50d0496a6de32196c2b65d32ca1494b1b81a
rmpestano <rmpestano@gmail.com>
2017-04-28 21:13:06 -0300

    fixes #10

---
+++ src/main/java/com/github/adminfaces/template/session/AdminServletContextListener.java
@@ -0,0 +1,27 @@
+package com.github.adminfaces.template.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.ResourceBundle;
+
+/**
+ * Created by rmpestano on 28/04/17.
+ */
+public class AdminServletContextListener implements ServletContextListener {
+    private static final Logger log = LoggerFactory.getLogger(AdminFilter.class.getCanonicalName());
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        log.info("Using Admin Template " + ResourceBundle.getBundle("admin").getString("admin.version"));
+        log.info("Using Admin Theme " + ResourceBundle.getBundle("admin-theme").getString("theme.version"));
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
