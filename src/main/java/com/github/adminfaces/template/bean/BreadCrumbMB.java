213db18e8046955b9a94481efa71b31cd4080310
rafael-pestano <rmpestano@gmail.com>
2018-03-23 08:56:54 -0300

    refs #52

--- src/main/java/com/github/adminfaces/template/bean/BreadCrumbMB.java
+++ src/main/java/com/github/adminfaces/template/bean/BreadCrumbMB.java
@@ -66,7 +66,7 @@ public class BreadCrumbMB implements Serializable {
         if(link != null && adminConfig.isExtensionLessUrls()) {
             link = link.substring(0, link.lastIndexOf("."));
         } else if(link != null && !link.contains(".")) {
-            link = link + "." + adminConfig.getIndexPage();
+            link = link + adminConfig.getPageSufix();
         }
         breadCrumb.setLink(link);
 
