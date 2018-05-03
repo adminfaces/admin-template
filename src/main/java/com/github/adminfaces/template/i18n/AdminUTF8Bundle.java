33f36dab1171f7137e5893e66f1e913e4e8e6b2e
rafael-pestano <rmpestano@gmail.com>
2018-05-03 17:05:15 -0300

    refs #65

--- src/main/java/com/github/adminfaces/template/i18n/AdminUTF8Bundle.java
+++ src/main/java/com/github/adminfaces/template/i18n/AdminUTF8Bundle.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.github.adminfaces.template.i18n;
+ package com.github.adminfaces.template.i18n;
 
 import java.io.IOException;
 import java.io.InputStream;
