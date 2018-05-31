a057a4802980c69b1b09ee8b19c2c86968af7baf
rmpestano <rmpestano@gmail.com>
2018-05-31 17:20:52 -0300

    refs #74

--- src/main/java/com/github/adminfaces/template/bean/LayoutMB.java
+++ src/main/java/com/github/adminfaces/template/bean/LayoutMB.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.logging.Logger;
+import org.omnifaces.util.Faces;
 
 /**
  * Created by rmpestano on 07/05/17.
@@ -12,7 +14,13 @@ import java.io.Serializable;
 @SessionScoped
 public class LayoutMB implements Serializable {
 
+    private static final Logger LOG = Logger.getLogger(LayoutMB.class.getName());
+    private static final String DEFAULT_TEMPLATE = "/admin.xhtml"; //template bundled in admin-template 
+    private static final String APP_TEMPLATE_PATH = "/WEB-INF/templates/template.xhtml"; // template provided by applications
+    private static final String APP_TEMPLATE_TOP_PATH = "/WEB-INF/templates/template-top.xhtml";
+
     private String template;
+    private Boolean appTemplateExists;
 
     private boolean defaultTemplateSelected;
 
@@ -20,21 +28,29 @@ public class LayoutMB implements Serializable {
     public void init() {
         setDefaultTemplate();
     }
-    
+
     public String getTemplate() {
         return template;
     }
 
     public void setTemplateTop() {
-        template = "/WEB-INF/templates/template-top.xhtml";
+         if (appTemplateExists()) {
+            template = APP_TEMPLATE_TOP_PATH;
+        } else {
+            template = DEFAULT_TEMPLATE;
+        }
     }
 
     public void setDefaultTemplate() {
-        template = "/WEB-INF/templates/template.xhtml";
+        if (appTemplateExists()) {
+            template = APP_TEMPLATE_PATH;
+        } else {
+            template = DEFAULT_TEMPLATE;
+        }
     }
 
     public void toggleTemplate() {
-        if(isDefaultTemplate()) {
+        if (isDefaultTemplate()) {
             setTemplateTop();
         } else {
             setDefaultTemplate();
@@ -42,7 +58,7 @@ public class LayoutMB implements Serializable {
     }
 
     public boolean isDefaultTemplate() {
-        return template != null && template.endsWith("template.xhtml");
+        return template != null && (template.endsWith("template.xhtml") || template.equals("admin.xhtml"));
     }
 
     public boolean isDefaultTemplateSelected() {
@@ -52,5 +68,19 @@ public class LayoutMB implements Serializable {
     public void setDefaultTemplateSelected(boolean defaultTemplateSelected) {
         this.defaultTemplateSelected = defaultTemplateSelected;
     }
-    
+
+    private boolean appTemplateExists() {
+        if (appTemplateExists != null) {
+            return appTemplateExists;
+        }
+        try {
+            appTemplateExists = Faces.getExternalContext().getResourceAsStream(APP_TEMPLATE_PATH) != null;
+        } catch (Exception e) {
+            LOG.warning("Could not find application defined template (), failing back to default admin template.");
+            appTemplateExists = false;
+        }
+
+        return appTemplateExists;
+    }
+
 }
