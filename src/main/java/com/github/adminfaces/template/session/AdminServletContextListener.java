5223f151477abbbff7d48d891090a0864e3a5c2b
rmpestano <rmpestano@oss-powerhouse>
2019-01-30 20:25:17 -0200

    fixes #153

--- src/main/java/com/github/adminfaces/template/session/AdminServletContextListener.java
+++ src/main/java/com/github/adminfaces/template/session/AdminServletContextListener.java
@@ -19,11 +19,15 @@ public class AdminServletContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             ResourceBundle adminBundle = ResourceBundle.getBundle("admin");
+            ResourceBundle adminPersistenceBundle = ResourceBundle.getBundle("admin-persistence");
             boolean isLegacyTemplate = has(adminBundle.getString("admin.legacy")) && adminBundle.getString("admin.legacy").equals("true");
             StringBuilder sb = new StringBuilder("Using Admin Template ")
                     .append(ResourceBundle.getBundle("admin").getString("admin.version"))
                     .append(isLegacyTemplate ? " (legacy)":"")
-                    .append(" and Admin Theme ").append(ResourceBundle.getBundle("admin-theme").getString("theme.version"));
+                    .append(" and Admin Theme ").append(ResourceBundle.getBundle("admin-theme").getString("theme.version"));         
+            if(has(adminPersistenceBundle)) {
+                sb.append(" and Admin Persistence ").append(adminPersistenceBundle.getString("admin-persistence.version"));
+            }
             log.log(Level.INFO,sb.toString());
         }catch (Exception e) {
             log.log(Level.WARNING,"Could not get AdminFaces version.",e);
