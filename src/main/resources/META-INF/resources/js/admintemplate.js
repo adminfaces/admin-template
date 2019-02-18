b223cfb8926a46ebbbbc328dcb8ba6dd222c5e5d
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-02-18 10:02:55 -0300

    refs #155

--- src/main/resources/META-INF/resources/js/admintemplate.js
+++ src/main/resources/META-INF/resources/js/admintemplate.js
@@ -631,3 +631,30 @@ PrimeFaces.locales['zh_CN'] = {
     day: '日',
     allDayText : '全天'
 };
+
+PrimeFaces.locales ['de'] = {
+    closeText: 'Schließen',
+    prevText: 'Zurück',
+    nextText: 'Weiter',
+    monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' ],
+    monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ],
+    dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    dayNamesShort: ['Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam'],
+    dayNamesMin: ['S', 'M', 'D', 'M ', 'D', 'F ', 'S'],
+    weekHeader: 'Woche',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix:'',
+    timeOnlyTitle: 'Nur Zeit',
+    timeText: 'Zeit',
+    hourText: 'Stunde',
+    minuteText: 'Minute',
+    secondText: 'Sekunde',
+    currentText: 'Aktuelles Datum',
+    ampm: false,
+    month: 'Monat',
+    week: 'Woche',
+    day: 'Tag',
+    allDayText: 'Ganzer Tag'
+};
