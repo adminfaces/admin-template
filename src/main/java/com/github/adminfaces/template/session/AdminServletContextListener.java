404e3b2b6424155cc4c00213703d3ee26449b94f
rmpestano <rmpestano@oss-powerhouse>
2019-03-23 23:58:36 -0300

    perf

--- src/main/java/com/github/adminfaces/template/session/AdminServletContextListener.java
+++ src/main/java/com/github/adminfaces/template/session/AdminServletContextListener.java
@@ -1,6 +1,5 @@
 package com.github.adminfaces.template.session;
 
-
 import static com.github.adminfaces.template.util.Assert.has;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -13,8 +12,8 @@ import java.util.logging.Logger;
  * Created by rmpestano on 28/04/17.
  */
 public class AdminServletContextListener implements ServletContextListener {
-    private static final Logger log = Logger.getLogger(AdminServletContextListener.class.getName());
 
+    private static final Logger log = Logger.getLogger(AdminServletContextListener.class.getName());
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
@@ -23,20 +22,21 @@ public class AdminServletContextListener implements ServletContextListener {
             ResourceBundle adminPersistenceBundle = null;
             try {
                 adminPersistenceBundle = ResourceBundle.getBundle("admin-persistence");
-            }catch (MissingResourceException mre) {
+            } catch (MissingResourceException mre) {
                 //intentional
             }
             boolean isLegacyTemplate = has(adminBundle.getString("admin.legacy")) && adminBundle.getString("admin.legacy").equals("true");
             StringBuilder sb = new StringBuilder("Using Admin Template ")
-                    .append(ResourceBundle.getBundle("admin").getString("admin.version"))
-                    .append(isLegacyTemplate ? " (legacy)":"")
-                    .append(" and Admin Theme ").append(ResourceBundle.getBundle("admin-theme").getString("theme.version"));         
-            if(has(adminPersistenceBundle)) {
-                sb.append(" and Admin Persistence ").append(adminPersistenceBundle.getString("admin-persistence.version"));
+                .append(ResourceBundle.getBundle("admin").getString("admin.version"))
+                .append(isLegacyTemplate ? " (legacy)" : "");
+
+            if (has(adminPersistenceBundle)) {
+                sb.append(", Admin Persistence ").append(adminPersistenceBundle.getString("admin-persistence.version"));
             }
-            log.log(Level.INFO,sb.toString());
-        }catch (Exception e) {
-            log.log(Level.WARNING,"Could not get AdminFaces version.",e);
+            sb.append(" and Admin Theme ").append(ResourceBundle.getBundle("admin-theme").getString("theme.version"));
+            log.log(Level.INFO, sb.toString());
+        } catch (Exception e) {
+            log.log(Level.WARNING, "Could not get AdminFaces version.", e);
         }
     }
 
